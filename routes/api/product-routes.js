15b250fa41301633f8e56b6bca7185d6ba24800c
hanandadras <hanan.aabid@gmail.com>
2022-03-07 10:37:56 -0800

    updated routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -5,14 +5,47 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 
 // get all products
 router.get('/', (req, res) => {
-  // find all products
-  // be sure to include its associated Category and Tag data
+  // find all products  // be sure to include its associated Category and Tag data
+  Product.findAll({
+    inculde: [
+      Category,
+      {
+        model: Tag,
+        through:ProductTag
+      }
+    ]
+  })
+  .then(products =>res.json(products))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
+ 
 });
+
+
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  Product.findOne({
+    where: {
+      id:req.params.id
+    },
+    include: [
+      Category,
+      {
+        moderl: Tag,
+        through: ProductTag
+      }
+    ]
+  })
+  .then(products =>res.json(products))
+  .catch(err=> {
+    console.log(err);
+    res.status(400).json(err);
+  });
 });
 
 // create new product
@@ -28,7 +61,7 @@ router.post('/', (req, res) => {
   Product.create(req.body)
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
-      if (req.body.tagIds.length) {
+      if (req.body.tagIds && req.body.tagIds.length) {
         const productTagIdArr = req.body.tagIds.map((tag_id) => {
           return {
             product_id: product.id,
@@ -89,8 +122,23 @@ router.put('/:id', (req, res) => {
     });
 });
 
+
 router.delete('/:id', (req, res) => {
-  // delete one product by its `id` value
+    // delete one product by its `id` value
+  Product.destroy({
+    where: {
+      id: req.params.id
+    }
+  }) 
+  .then(products=>{
+    console.log(products);
+    res.json(products);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(400).json(err);
+  })
+
 });
 
 module.exports = router;
